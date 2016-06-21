43814235a63062ab88560553c802d8a06af43f6b
Tim Reichelt <treichelt6@gmail.com>
2016-06-21 13:53:17 +0200

    refactor index and login js into new file

---
+++ server/static/js/login.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#login-btn").on('click', function(){
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var loginParams = {username: username, password: password};
+        var loginURL = "../auth/0.1/login?";
+        $.get({
+            url: loginURL,
+            data: loginParams
+        }).done(function(data, textStatus, jqXHR) {
+            if (data == "Success") {
+                window.location = "../dashboard";
+            }
+        }).fail(function() {
+            console.log("Wrong password!");
+            $("#alert-wrong-password").html('<div class="alert alert-danger">Wrong password or username.</div>');
+            return false;
+        });
+    });
+});
+// Also log in when hit enter
+$(document).ready(function() {
+    $(document).keypress(function(e) {
+        var enter = 13;
+        var key = e.which;
+        if (key == enter) {
+            $("#login-btn").click();
+            return false;
+        }
+    });
+});
