655a641431a25e924284a1fdcc61342730da15df
Tim Reichelt <treichelt6@gmail.com>
2016-06-27 10:27:23 +0200

    change chart style

--- server/static/js/dashboard.js
+++ server/static/js/dashboard.js
@@ -33,6 +33,15 @@ var batteryData = $.get({
             datasets: [{
                 label: "Moto X",
                 borderColor: "rgba(75,192,192,1)",
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
                 data: batteryLevel
             }]
         },
