6758406260dc3f798df006313a8c2d4089c2cbf4
Tim Reichelt <treichelt6@gmail.com>
2016-08-08 14:26:53 +0200

    delete basic auth middleware

--- server/middlewares/authentication/authentication.go
+++ server/middlewares/authentication/authentication.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/michaelg9/ISOC/server/controllers"
 	"github.com/michaelg9/ISOC/server/models"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -19,32 +17,6 @@ const (
 // MiddlewareEnv embeds the controllers.Env struct so that we can write functions on it.
 type MiddlewareEnv struct {
 	*controllers.Env
-}
-
-// RequireBasicAuth is the middleware for routes that require HTTP Basic authentication.
-func (env *MiddlewareEnv) RequireBasicAuth(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	email, password, ok := r.BasicAuth()
-	if !ok || email == "" || password == "" {
-		http.Error(w, errNotAuthorized, http.StatusUnauthorized)
-		return
-	}
-
-	// Check if user is registered in database
-	user, err := env.DB.GetUser(models.User{Email: email})
-	if err != nil {
-		http.Error(w, errNotAuthorized, http.StatusForbidden)
-		return
-	}
-
-	// Check if given password fits with stored hash inside the server
-	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
-	if err != nil {
-		http.Error(w, errNotAuthorized, http.StatusForbidden)
-		return
-	}
-
-	// User is authorised and can proceed
-	next(w, r)
 }
 
 // RequireSessionAuth is the middleware for routes that require a session to be set with an email.
--- server/routers/routes.go
+++ server/routers/routes.go
@@ -7,7 +7,6 @@ import (
 )
 
 const (
-	basicAuth   = "Basic"
 	sessionAuth = "Session"
 	tokenAuth   = "Token"
 )
